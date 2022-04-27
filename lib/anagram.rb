8abe69aabb0e5027293389aefbd005d496d699cb
Throsby Wells <throsbywells@gmail.com>
2022-04-27 15:27:19 -0400

    commit tree e7d22848a460b58d9dcea1d968defbf74077953f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :str
+    def initialize(str)
+        self.str = str
+    end
+
+    def match(arr)
+        arr_return = []
+        arr.each do |word|
+            p word, self.str
+            # p word.split("")
+            # p self.str.split("")
+            if word.chars.sort == self.str.chars.sort
+                arr_return << word
+            end
+        end
+        return arr_return
+    end
+end
+
+ba = Anagram.new('ba')
+p ba.match(["ab", "frappe"])
